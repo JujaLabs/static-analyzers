6d89d09162b14e9a009e0f396dcdf71b12fbbb5f
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-03-29 18:18:39 +0300

    step3:braces

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -24,23 +24,27 @@ class Customer {
             switch (each.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if (each.getDaysRented() > 2)
+                    if (each.getDaysRented() > 2) {
                         thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
                     break;
                 case Movie.NEW_RELEASE:
                     thisAmount += each.getDaysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
                     thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
+                    if (each.getDaysRented() > 3) {
                         thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
                     break;
             }
             // добавить очки для активного арендатора
             frequentRenterPoints++;
             // бонус за аренду новинки на два дня
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                && each.getDaysRented() > 1) frequentRenterPoints++;
+                && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
             //показать результаты для этой аренды
             result += "\t" + each.getMovie().getTitle() + "\t" +
                 String.valueOf(thisAmount) + "\n";
